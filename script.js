eb85fcb600ebc81bf811e6f95f526840d31e6d0b
alberto araújo <91097353+albertoaraujoo@users.noreply.github.com>
2022-06-02 13:43:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,13 @@
 
 let meatAd = 150;
 let meatCh = 50;
-let beerValue = 660;
-let waterValue = 500;
+let beerValue = 350;
+let waterValue = 350;
 let sodaValue = 300;
 
 let children = document.getElementById('chInput');
 let adults = document.getElementById('adInput');
 let duration = document.getElementById('duration');
-
 
 
 
@@ -23,33 +22,57 @@ button.addEventListener('click', () => {
     
         if (meat < 1.0){
             document.getElementById("meat").innerHTML = 
-                `<img class="results-img" src="./assets/carne.png" >
-                 ${parseFloat(meat)} gramas de carne.`
+                `<img class="results-img" src="./assets/meat.png" >
+                 ${parseFloat(meat*1000)} g de carne.`
     } 
         else{
             document.getElementById("meat").innerHTML = 
-            `<img class="results-img" src="./assets/carne.png" >
-            ${parseFloat(meat)} kgs de carne.`
+            `<img class="results-img" src="./assets/meat.png" >
+            ${parseFloat(meat)} kg de carne.`
     }
 
 
-    let beer = (beerValue * adults.value) * duration.value;
 
-        document.getElementById("beer").innerHTML =
-        `<img class="results-img" src="./assets/cerveja.png">
+    let beer = ((beerValue * 2) * adults.value) * duration.value;
+
+        if (beer < 350){
+            document.getElementById("beer").innerHTML =
+        `<img class="results-img" src="./assets/beer.png">
+        ${parseInt(beer / 350)} latinha de cerveja.`
+        } else{
+            document.getElementById("beer").innerHTML =
+        `<img class="results-img" src="./assets/beer.png">
         ${parseInt(beer / 350)} latinhas de cerveja.`
+        }
+
+        
 
     let water = ((waterValue * adults.value * duration.value) + (waterValue * children.value * duration.value)) / 1000 ;
 
+       if (water < 1.0){
         document.getElementById("water").innerHTML = 
-        `<img class="results-img" src="./assets/garrafa-de-agua.png"> 
+        `<img class="results-img" src="./assets/water.png"> 
+        ${water*1000} ml de água.`
+       } else{
+        document.getElementById("water").innerHTML = 
+        `<img class="results-img" src="./assets/water.png"> 
         ${water} litros de água.`
+       }
+        
     
     let soda = ((sodaValue * adults.value * duration.value) + (sodaValue * children.value * duration.value)) / 1000 ;
 
-         document.getElementById("soda").innerHTML = 
-         `<img class="results-img" src="./assets/soda.png"> 
-         ${soda} litros de Refrigerante.`
+         if (soda < 1.0){
+            document.getElementById("soda").innerHTML = 
+            `<img class="results-img" src="./assets/soda.png"> 
+            ${soda*1000} ml de Refrigerante.`
+         } else {
+            document.getElementById("soda").innerHTML = 
+            `<img class="results-img" src="./assets/soda.png"> 
+            ${soda} litros de Refrigerante.`
+         }
+
+         console.log(soda)
 
          document.querySelector('.results').style.display = "flex";
 })
